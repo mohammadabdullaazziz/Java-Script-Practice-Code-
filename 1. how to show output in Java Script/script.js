5d64e40755c622f999eac0c68c5bcce764392f26
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-29 07:31:35 -0700

    how to show output in Java Script

---
+++ 1. how to show output in Java Script/script.js
@@ -0,0 +1,6 @@
+alert("Bangladesh Travel");
+
+document.write("<strong>Name :</strong> Abdullah Arman </br>");
+document.write("<strong>Address :</strong> Rangpur, Bangladesh </br>");
+document.write("<strong>Phone :</strong> <em>01568451112</em?");
+
