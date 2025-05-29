bd1806a98798518d5683c878d21dc789dc243f45
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-29 08:19:55 -0700

    Java script Statement

---
+++ 3. Java script Statement/script.js
@@ -0,0 +1,8 @@
+alert("Bangladesh World");
+console.log("Abdullah");
+document.write("Abdullah");
+
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
