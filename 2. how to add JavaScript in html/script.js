c4237e399556782e0524f416c682b532e2ad9cf1
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-29 08:00:40 -0700

    how to add JavaScript in html

---
+++ 2. how to add JavaScript in html/script.js
@@ -0,0 +1,8 @@
+alert("Abdullah Arman");
+
+document.write("<strong>Name :</strong> Abdullah Arman </br>");
+document.write("<strong>Address :</strong> Rangpur, Bangladesh </br>");
+document.write("<strong>Phone :</strong> <em>01568451112</em?");
+
+console.log("Abdullah" + " " + "Aziz");
+
