8d4f56720b1623fa2cbe764c902d55b39c89776d
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-29 10:31:47 -0700

    Java script Constant Const Vs Let

---
+++ 7. Javascript Constant  Const Vs Let/script.js
@@ -0,0 +1,16 @@
+let myName = "Abdullah";
+myName = "Arman";
+let myFriendName = "Biplob";
+console.log(myName, myFriendName);
+
+// Const Variable
+
+const flower = "Rose";
+flower = "Sunflower"
+console.log(flower);
+
+const FLOWER = "Rose";
+console.log(FLOWER);
+
+const IP_ADDRESS = "192.1.0.1";
+console.log(IP_ADDRESS);
