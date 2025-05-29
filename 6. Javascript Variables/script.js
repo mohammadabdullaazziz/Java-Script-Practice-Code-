77bcaa39a475753bbf2a5e64730ce06899b5a892
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-29 10:04:15 -0700

    Java script Variables

---
+++ 6. Javascript Variables/script.js
@@ -0,0 +1,93 @@
+
+var x = 10;
+// console.log(x);
+x = 20;
+// console.log(x);
+x = 50;
+console.log(x);
+
+const x = 5;
+const y = 6;
+const z = x + y;
+console.log(z);
+
+let x1 = 5;
+let y1 = 6;
+let z1 = x1 + y1;
+console.log(z1);
+
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+
+01. abdullah_bin_Aziz (Snake Case);
+
+02. abdullahBinAziz (Camale Case);
+
+03. AbdullahBinAziz (Pascal Case);
+
+// again repeat how to declare variable with string number boolean
+
+let name2 = "Abdullah Arman";
+// console.log(name);
+name2 = "Arman Aziz";
+// console.log(name);
+name2 = "Rokshana Aziz"
+console.log(name2);
+
+
+
+var name = "Abdullah Arman";
+// document.write(name);
+name = "Aziz </br>";
+var age = 29;
+document.write(name,age);
+
+var name;
+var country;
+var age;
+name = "Mohammad </br>"
+country = "Bangladesh </br>"
+age = 56;
+document.write(name,country,age);
+var name,country,age;
+
+
+var name = "Abdullah ", 
+country = "Bangladesh ", 
+age = 29;
+console.log(name);
+
+// string number boolean typeof operators code
+
+var name = "Abdullah Arman";
+var age = 29;
+console.log(name,age);
+
+var name = typeof 'Abdullah "Arman';
+var age = typeof 29;
+console.log(name,age)
+
+var name = typeof "Azziz";
+var age = typeof 28;
+console.log(name,age);
+
+boolean data type...
+// false=0
+// true=1
+
+var isSubs = typeof true;
+var isPassed = typeof false;
+console.log(isSubs,isPassed);
+
+console.log(3>2);
+console.log(10<5);
+console.log(5<10);
+
+
