5d9d3cc1fafa703b1b066cd4cf9591a03e1d2347
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-30 08:24:48 -0700

    Number Data Type

---
+++ 10. Number Data Type/script.js
@@ -0,0 +1,28 @@
+<h3>Number Data Type</h3>
+
+let length = 16; 
+let weight = 7.5;
+
+let x1 = 3.14;    // A number with decimals
+let y1 = 3;       // A number without decimals
+
+let x2 = 100;         // x is a number
+
+let price = 250;
+price = "Hello"; //Dynamic Type Variable  //But Kora Jabe Na
+console.log(price)
+
+<h4>Number Type Value</h4>
+1. Infinity
+2. NaN
+
+let age = 25;
+let age2 = 0;
+console.log(age / age2);  //Infinity
+
+console.log("hello" / 2); //NaN
+
+<h4>BigInt Type Variable</h4>
+
+let result = 2146574325356474257362547
+365n
