1df152fb2dcbab1f988af536fd035791e2f474a5
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-30 09:49:30 -0700

    Java Script Boolean Data Type

---
+++ 12. Java Script Boolean Data Type/script.js
@@ -0,0 +1,13 @@
+<h3>Booleans Data Type</h3> 
+
+let x = true;
+let y = false; 
+
+let numOne = Number(true);
+let numTwo = Number(false);
+console.log(numOne);
+console.log(numTwo);
+
+console.log(3 > 2);
+console.log(3 < 2);
+
