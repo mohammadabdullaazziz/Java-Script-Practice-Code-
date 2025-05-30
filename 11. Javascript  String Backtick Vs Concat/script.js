8ec5c799745dc4f8aec5a9edf025a5eed945143c
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-30 09:34:40 -0700

    Java script String Backtick Vs Concat

---
+++ 11. Javascript  String Backtick Vs Concat/script.js
@@ -0,0 +1,44 @@
+<h3>String Data Type</h3> 
+
+let name1 = "It's alright";
+let name2 = "He is called 'Abdullah'";
+let name3 = 'He is called "Abdullah"';
+
+let myName1 = "Abdullah";  // Double quotes
+let myName2 = 'Abdullah';  // Single quotes
+
+''	Single quote
+""	Double quote
+\   Backslash
+
+let text1 = "We are the so-called \"Abdullah\" from the Bangladesh.";
+
+let text2= 'It\'s alright.';
+
+let text3 = "The character \\ is called backslash.";
+
+ 
+let text = "Bangladesh";
+let length = text.length;
+console.log(length);
+
+<h3>backticks</h3> 
+let text4 = `He's often called "Abdullah"`;
+console.log(text4);
+
+let mangoPrice = 20;
+let name = `Abdullah`;
+console.log(`name ${mangoPrice}`);
+
+let userName = "Abdullah";
+let year = 29;
+console.log('Hello ${userName}, Your Age is ${year}');
+
+/*
+Code	Result
+\b	    Backspace
+\f	    Form Feed
+\n	    New Line
+\r	    Carriage Return
+\t	    Horizontal Tabulator
+\v	    Vertical Tabulator */
