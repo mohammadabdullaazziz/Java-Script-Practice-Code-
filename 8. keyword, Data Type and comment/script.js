eb64821467d8810a60c8d15abcfb084003eeab7f
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-30 07:20:16 -0700

    keyword, Data Type and comment

---
+++ 8. keyword, Data Type and comment/script.js
@@ -0,0 +1,36 @@
+<h3>Java Script Reserved Words</h3>
+
+abstract  arguments  await*  boolean
+break  byte  case  catch
+char  class*  const*  continue
+debugger default	 delete  do
+double  else  enum*  eval
+export*  extends*  false  final
+finally  float  for  function 
+goto	 if  implements  import*
+in  instanceof  int  interface
+let*  long  native	new
+null package	private	 protected
+public  return  short  static
+super*  switch  synchronized  this
+throw  throws	transient  true
+try  typeof  var  void
+volatile  while  with  yield */
+
+<h3>Data Type</h3> 
+// primitives data type
+// 1.String - "Abdullah Arman"
+// 2.Number - 29,  123.23
+// 3.Boolean - true,false (value)
+// 4.null
+// 5.undefined
+// 6.symbol
+
+// complex data type
+// 1. function
+// 2. object
+// 3. array
+
+<h3>2 comment in js</h3>
+/* 1.  multiple line comment */ 
+// 2. singal line comment
