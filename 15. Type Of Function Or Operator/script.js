efa92231afeae8ec2a0e7108aadb6bb8505c708a
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-31 07:56:04 -0700

    Type Of Function Or Operator

---
+++ 15. Type Of Function Or Operator/script.js
@@ -0,0 +1,19 @@
+<h3>Type Of Function Or Operator</h3>
+
+typeof //Type Of Operator
+typeof() //Type Of Function
+
+let name = "Abdullah";
+console.log(typeof name); //string data type
+
+let price = 30;
+console.log(typeof(price)); //Number data type
+
+console.log(typeof true); //Boolean data type
+console.log(typeof false); //Boolean data type
+
+
+console.log(typeof undefined); //undefined data type
+console.log(typeof null); // object 
+
+console.log(typeof 12n); // BigInt
