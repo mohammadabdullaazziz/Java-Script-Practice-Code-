dd6ef7e9582052316b40b33b9647ab0adbb6c80e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-06-01 07:42:33 -0700

    java script Operator, Binary, Unary

---
+++ 18.  java script Operator, Binary, Unary/index.js
@@ -0,0 +1,52 @@
+<h3>JavaScript Arithmetic Operators</h3>
+
+let mangoPrice = 40;
+let applePrice = 20;
+console.log(mangoPrice + applePrice);   addition + operator
+console.log(mangoPrice - applePrice);   subtraction - operator
+console.log(mangoPrice * applePrice);   multiplication * operator
+console.log(mangoPrice / applePrice);   division / operator
+console.log(mangoPrice % applePrice);   modulus % operator
+
+++	Increment
+--	Decrement
+// ** exponent
+console.log(5 ** 2);
+
+let num = "100";
+console.log(typeof +num); Number 
+
+// Binary operator
+let mango = 40;
+let apple = 20;
+console.log(mango + apple); 
+console.log(mango - apple);   
+console.log(mango * apple);   
+console.log(mango / apple);   
+console.log(mango % apple); 
+
+let firstNumber = 8;
+let secondNumber = 2;
+let result = firstNumber ** secondNumber; // 	Exponentiation operator (ES2016)
+console.log(result);
+
+Operand	Operator Operand
+100	      +	     50
+
+
+// unary operator
+
+// console.log(-a);  negation operator
+// console.log(a+);  unary + operator
+
+let price1 = 20;
+let price2 = -price1;
+console.log(price2);
+
+let fNumber = 5;
+let sNumber = -fNumber;
+console.log(sNumber);
+
+let match = 50;
+match = -match;
+console.log(match);
