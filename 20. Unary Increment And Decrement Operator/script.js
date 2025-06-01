ccce7dc38a31666d34060399bf57993383940854
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-06-01 09:01:52 -0700

    Unary Increment And Decrement Operator

---
+++ 20. Unary Increment And Decrement Operator/script.js
@@ -0,0 +1,56 @@
+<h3>Java Script Unary Increment and Decrement</h3>
+
+Increment ++  operator
+Decrement --  operator
+Syntax:
+a++
+OR
+++a
+
+let r = 10;
+console.log(r++);
+console.log(r);
+
+let m = 10;
+console.log(++m);
+console.log(m);
+
+a = 5
+++a;          // a becomes 6
+a++;          // a becomes 7
+--a;          // a becomes 6
+a--;          // a becomes 5
+
+Increment ++ operator
+Postfix a++
+Prefix ++a
+
+
+Decrement -- operator
+Postfix Decrement a--
+Prefix Decrement  --a
+
+
+Unary Increment (++) Operator
+//Postfix
+let x = 12;
+let y = x++;
+console.log(x);
+console.log(y);
+
+// Prefix
+x = 10;
+y = ++x;
+console.log(x);
+console.log(y);
+
+Unary Decrement (--) Operator
+let x = 8;
+let y = x--;
+console.log(x);
+console.log(y);
+
+x = 15;
+y = --x;
+console.log(x);
+console.log(y);
