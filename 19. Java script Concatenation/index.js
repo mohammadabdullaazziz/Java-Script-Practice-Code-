5d4b54d759ed2e2ad9e9b136e2066211993fc27e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-06-01 08:09:46 -0700

    Java script Concatenation

---
+++ 19. Java script Concatenation/index.js
@@ -0,0 +1,47 @@
+<h3>Javascript Concatenation</h3>
+
+var num1 = 20;
+var num2 = 30;
+document.write("Num1 = " + num1 + " and Num2 = " + num2);
+
+let firstName = "Abdullah";
+let lastName = "Aziz";
+let fullName = (firstName + " " + lastName);
+console.log(fullName);
+
+let result = 4 + "9";
+console.log(result);
+console.log(typeof result);
+
+let results = "9" + 4;
+console.log(results);
+console.log(typeof results);
+
+let result1 = 4 + 4 + "2";
+console.log(result1);
+console.log(typeof result1);
+
+let result2 = "2" + 2 + 4;
+console.log(result2);
+console.log(typeof result2);
+
+let result3 = 20 - "2";
+console.log(result3);
+console.log(typeof result3);
+
+let result4 = 20 * "2";
+console.log(result4);
+console.log(typeof result4);
+
+let result5 = 20 / "2";
+console.log(result5);
+console.log(typeof result5);
+
+let result6 = 20 % "2";
+console.log(result6);
+console.log(typeof result6);
+
+let result7 = 20 ** "2";
+console.log(result7);
+console.log(typeof result7);
+
