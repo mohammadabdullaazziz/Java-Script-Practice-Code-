3e3dab102aeef533e88ac23abce8b416481ebef0
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-03 21:26:18 +0600

    Area of various shapes

---
+++ 24.  Area of various shapes/script.js
@@ -0,0 +1,16 @@
+// Rectangle Base*Height
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = base * height;
+
+document.write("Area = " + area);
+
+// Triangle Base * Height/2
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = (base * height)/2;
+
+document.write("Area = " + area);
+
