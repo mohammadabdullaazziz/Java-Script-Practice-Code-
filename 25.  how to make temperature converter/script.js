328ee6587be7c47e9defd0a1cc22c8b00236037e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-03 21:49:13 +0600

    how to make temperature converter

---
+++ 25.  how to make temperature converter/script.js
@@ -0,0 +1,10 @@
+// fahrenheit to celsius
+var fahrenheit = parseFloat(prompt("Enter fahrenheit = "));
+var celsius = (fahrenheit - 32) * (5 / 9);
+
+document.write("Celsius = " + celsius)
+
+//celsius to  fahrenheit 
+var celsius = parseFloat(prompt("Enter celsius = "));
+var fahrenheit = (celsius * (9 / 5)) + 32;
+document.write("Fahrenheit = " + fahrenheit)
