53cf059939e83879a347915ea3494b263a74159d
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-03 20:52:45 +0600

    Java script Assignment Operators

---
+++ 23. Javascript Assignment Operators/script.js
@@ -0,0 +1,29 @@
+<h3>Javascript Assignment Operators</h3>
+
+Assignment operator
+
+let xyz = 10;
+
+// =,  +=, -=, *=, /=, %=; **=,
+
+let b = 6;
+b += 3; //x = x + 3;
+console.log(b)
+
+let y = 1;
+y -= 4; //y = y - 4;
+console.log(y);
+
+
+let z = 2;
+z *= 6; //z = z * 6;
+console.log(z);
+
+let x = 20;
+x /= 4; // x is now 5 (equivalent to x = x / 4)
+
+let m = 10;
+m %= 3; // m is now 1 (equivalent to x = x % 3)
+
+let k = 2;
+k **= 3; // k is now 8 (equivalent to x = x ** 3)
