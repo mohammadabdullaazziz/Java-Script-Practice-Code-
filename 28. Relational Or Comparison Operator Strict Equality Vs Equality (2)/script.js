3b13585954b9239dc12171fcccf80bff7cafc2dd
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-05 22:56:49 +0600

    Comparison Operator Strict Equality Vs Equality

---
+++ 28. Relational Or Comparison Operator Strict Equality Vs Equality (2)/script.js
@@ -0,0 +1,26 @@
+
+console.log(20 === "20"); false
+console.log(20 === 20);  true
+console.log("20" === "20"); true
+console.log(20 === 30); false
+console.log(undefined === undefined); true
+console.log(null === null); true
+console.log("" === " "); false
+console.log("" === ""); true
+console.log(true === 1); false
+console.log(false === 0); false
+
+let num = Number("100");
+console.log(num === 10); false
+
+console.log("100" !== "100");  false
+console.log(100 !== 100);  false
+console.log(100 !== 120);  true
+console.log("100" !== 120);  true
+console.log(null !== null); false
+console.log(undefined !== null); true
+
+
+
+
+
