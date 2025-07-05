fd9c706d09ec892b0be9118041ee770a2bf23f8c
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-05 22:04:15 +0600

    Relational Or Comparison Operator (1)

--- 27. Relational Or Comparison Operator(1)/script.js
+++ 27. Relational Or Comparison Operator(1)/script.js
@@ -2,10 +2,13 @@
 // let num2 = 25;
 // console.log(num1 > num2); false
 
+// console.log("65" > "65"); false
 
 // let num1 = 20;
 // let num2 = 25;
 // console.log(num1 < num2); true
+
+// console.log("65" < 65); false
 
 
 // let num1 = 20;
@@ -18,16 +21,25 @@
 
 
 // console.log(20==20); true
-// console.log(20==40); false
-
 // console.log(20=="20"); true
 
+// console.log(20==40); false
+// console.log("20"== 40); false
 
-// console.log(20==="20"); false
-// console.log(20===30); false
+// console.log(0 == false); true
+// console.log(1 == true); true
+
+// console.log(true == true); true
+// console.log(false == false); true
+// console.log(null == null); true
+// console.log("Abdullah" == "Abdullah"); true
+// console.log("01" == 1); true
+// console.log("Hello" == 123); false
 
 
-// console.log(20!=30); true
+// console.log(20 != "30"); true
+// console.log(25 != 20); true
+// console.log(25 != 25); false
+// console.log(null != null); false
+// console.log("Hello" != 123); true
 
-// console.log(25!=20); true
-// console.log(25!=25); false
