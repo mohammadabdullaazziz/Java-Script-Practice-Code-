614d5eb2b26581d954674aef2c2fb693395ca621
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-06 23:43:09 +0600

    Else if-Statement In Java script

---
+++ 31. Else if-Statement In Javascript/index.js
@@ -0,0 +1,62 @@
+let monthName = prompt("Enter Month Name First 3 Word :")
+if (monthName == "Jan") {
+  console.log("January")
+} else if (monthName == "Feb") {
+  console.log("February");
+} else if (monthName == "Mar") {
+  console.log("March");
+} else if (monthName == "Apr") {
+  console.log("April");
+} else if (monthName == "May") {
+  console.log("May");
+} else if (monthName == "Jun") {
+  console.log("June");
+} else if (monthName == "Jul") {
+  console.log("July");
+} else if (monthName == "Aug") {
+  console.log("August");
+} else if (monthName == "Sep") {
+  console.log("September");
+} else if (monthName == "Oct") {
+  console.log("October");
+} else if (monthName == "Nov") {
+  console.log("November");
+} else if (monthName == "Dec") {
+  console.log("December");
+} else {
+  console.log("BYE");
+}
+
+let amount = 21;
+if (amount == 10) {
+    console.log("Hello");
+} else {
+    console.log("Bye");
+}
+// Output: "Bye"
+
+let amount2 = 21;
+if (amount2 == 10) {
+    console.log("Hello");
+} else if (amount2 = 21) {
+    console.log("21");
+} 
+// Output: "21"
+
+let speed = 87;
+if (speed > 90) {
+  console.log("OverSpeeding");
+} else if (speed > 50) {
+  console.log("Normal");
+} else {
+  console.log("Slow");
+}
+// Output: "Normal"
+let x = 5;
+let y = 8;
+if(x == y){
+console.log('IF statement executed');
+} else {
+console.log("condition not true.ELSE statement executed");
+}
+
