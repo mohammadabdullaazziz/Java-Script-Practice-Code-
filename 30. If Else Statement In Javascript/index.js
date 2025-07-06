51aee6db116b3c5778c4fa3d2c3b44aa92d1016b
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-06 22:48:32 +0600

    If Else Statement In Java script

---
+++ 30. If Else Statement In Javascript/index.js
@@ -0,0 +1,66 @@
+let mangoPrice = 20;
+let bananaPrice = 20;
+if (mangoPrice <= bananaPrice) {
+    console.log("Its True");
+}
+
+let myName = "Abdullah";
+
+if (myName == "Abdullah") {
+    console.log("Hello");
+ }
+
+if (3 < 5) {
+    console.log("Yes True");
+}
+
+let name = "Aziz";
+    if (name == "Abdullah") {
+    console.log("is false");
+}
+
+let a;
+
+if (a = "Abdullah") {
+    console.log("is True");
+}
+
+let b;
+if (b = null) {
+    console.log("is false");
+ }
+
+let myColor = "Orange";
+if (myColor == "Orange") {
+    console.log("Just like the Orange !");
+}
+
+let c;
+
+if ( c = "Hello" ) {
+    console.log("C Is True");
+}
+
+let d;
+
+if ( d = null ) {
+    console.log("C Is False");
+}
+
+let e;
+if (e = "Abdullah") {
+    console.log("My Name Is Abdullah");
+}
+
+if ("0") {
+    console.log("This is True")
+}
+
+if ( 3 + 5 ) {
+    console.log("Its True")
+}
+
+let amount = prompt("Enter Your Amount: ");
+if (amount = "100") {
+    console.log("Abdullah");
+}
