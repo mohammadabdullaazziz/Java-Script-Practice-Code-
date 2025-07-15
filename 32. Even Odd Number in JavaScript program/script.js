ac5d4257f1b2c3d43d70b23ff6656ba2625aba28
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-07-15 21:57:39 +0600

    Even Odd Number in Java Script program

---
+++ 32. Even Odd Number in JavaScript program/script.js
@@ -0,0 +1,10 @@
+let num = prompt("Enter Your Number");
+num = Number(num);
+
+// let remainder = num % 2;
+
+if ( (num % 2) == 0) {
+    console.log(num + " is Even Number")
+} else {
+    console.log(num + " is Odd Number");
+}
