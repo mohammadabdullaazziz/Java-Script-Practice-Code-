0b62bcb7fae220b070817c659fa28c597ca456dc
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-08-09 21:20:51 +0600

    Update script.js

--- 38. Javascript While Loop/script.js
+++ 38. Javascript While Loop/script.js
@@ -7,3 +7,15 @@ while ( count < 5) {
     console.log(count)
     count++ 
 }
+
+let amount = 10;
+while (amount >= 1) {
+   console.log(amount);
+   amount--;
+}
+
+let score = 0;
+while (score <= 10) {
+    console.log(score);
+    score += 2;
+}
