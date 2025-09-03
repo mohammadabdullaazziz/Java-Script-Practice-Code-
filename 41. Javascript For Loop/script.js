504d2310758a2949a26cb14f0fc25798fe280b80
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-09-03 21:57:33 +0600

    Java script For Loop

---
+++ 41. Javascript For Loop/script.js
@@ -0,0 +1,29 @@
+// For loop syntax
+
+for (initialization; test condition; iteration statement){
+    Statements to be executed if test condition is true
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log("Hello, world!");
+}
+
+// Output:
+// Hello, world!
+// Hello, world!
+// Hello, world!
+
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration number: " + i);
+}
+
+
+for (let i = 0; i < 5; i++) {
+  console.log("Hello");
+}
+
+let count;
+for ( count = 1; count <= 5; count++) {
+  console.log("Hello " + count);
+}
+console.log(count);
