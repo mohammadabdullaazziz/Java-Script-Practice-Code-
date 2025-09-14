cb7d0d7c248560c1d8d04109d4fcac651c92fbee
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-09-14 23:48:49 +0600

    Java Script Array

---
+++ 44. JavaScript Array/script.js
@@ -0,0 +1,10 @@
+// Java Script Array
+var tutorial = ['HTML', 'CSS', 'JavaScript', 'PHP'];
+console.log(tutorial[0]);
+
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits[2]);
+
+let friendAge = [20, 25, 27, 29, 30];
+console.log(friendAge[2]);
+
