e7766f9b0f4dffb4ef6954456743fc8e79ceb505
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-10-11 20:49:43 +0600

    problem solving for loop

--- 41. Javascript For Loop/Problem Solving With  For Loop/app.js
+++ 41. Javascript For Loop/Problem Solving With  For Loop/app.js
@@ -5,7 +5,7 @@ for (let number = 0; number <= 10; number++) {
     sum = sum + number;
 }
 console.log("Sum", sum);
-
+----------------------------------
 //decremental for loop
 let add = 10;
 for (let number = 10; number >= 0; number--){
@@ -13,3 +13,22 @@ for (let number = 10; number >= 0; number--){
     add = add - number;
 }
 console.log(add);
+----------------------------
+
+for (let i = 0; i <= 10; i++){
+    if (i % 2 == 0 ){
+      console.log(i);
+    } 
+}
+------------------------
+for (let i = 0; i <= 10; i++){
+    if (i % 2 !== 0 ){
+      console.log(i);
+    }  
+}
+--------------------------
+for (let i = 0; i <= 30; i++){
+    if (i % 3 == 0 || i % 5 == 0){
+      console.log(i);
+    }  
+}
