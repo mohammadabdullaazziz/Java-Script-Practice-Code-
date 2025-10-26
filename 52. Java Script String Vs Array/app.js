026cd766215995d06d9c080b21a2d6dd4aa42f2e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-10-26 19:50:10 +0600

    Java Script String Vs Array

---
+++ 52. Java Script String Vs Array/app.js
@@ -0,0 +1,20 @@
+// java script string
+let thana = new String('Gongachara');
+// console.log(typeof thana);
+
+let text = "Bangladesh";
+// console.log(text.length);
+console.log(text[0]);
+text[1] = "K";
+console.log(text); //immutable (not changeable)
+
+
+
+
+// String vs Array
+
+let numbers = [25, 55, 75, 85, 95, 100];
+// console.log(numbers.length); 
+console.log(numbers[3]);
+numbers [1] = 11;
+console.log(numbers);
