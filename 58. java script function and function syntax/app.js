1cbea75186a9fdea8a7f3e6bc963f1cfe5028758
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-11-01 21:05:00 +0600

    java script function and function syntax

---
+++ 58. java script function and function syntax/app.js
@@ -0,0 +1,19 @@
+//Syntax:
+function functionName(parameters) {
+  // Function body
+}
+
+
+function myNames (name, country) {
+  console.log("My name is" + " " + name + " " + "I'm come from" + " " + country)
+}
+
+myNames("Abdullah", "Bangladesh");
+myNames("Aziz", "Pakistan");
+
+function myIntro () {
+  console.log("I am Abdullah");
+  console.log("My age 28");
+  console.log("I live in Bangladesh");
+}
+myIntro();
