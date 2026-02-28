737f6d7f7f61fab83c9f8418cba6c401b195d77b
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-02-28 15:26:44 +0600

    Update script.js

--- 36. Javascript Logical ! NOT Operator/script.js
+++ 36. Javascript Logical ! NOT Operator/script.js
@@ -43,4 +43,90 @@ console.log(!0); // Output: true (0 is falsy, !0 is true)
 console.log(!"hello"); // Output: false ("hello" is truthy, !"hello" is false)
 console.log(!null); // Output: true (null is falsy, !null is true)
 console.log(!undefined); // Output: true (undefined is falsy, !undefined is true)
+
 console.log(!!0); // Output: false (double negation converts to actual boolean value)
+
+
+
+// let isLoggedIn = false;
+
+// // "!" মানে হলো "যদি না হয়"
+// if (!isLoggedIn) {
+//     console.log("দয়া করে আগে লগইন করুন।");
+// } else {
+//     console.log("স্বাগতম! আপনি ড্যাশবোর্ড দেখতে পারেন।");
+// }
+
+
+// let userName = "";
+
+// if (!userName) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let score = 100;
+// console.log(!!score); // আউটপুট: true (কারণ ১০০ একটি সত্য মান)
+
+// let empty = "";
+// console.log(!!empty); // আউটপুট: false (কারণ খালি স্ট্রিং মিথ্যা মান)
+
+
+let isLightOn = false;
+// সুইচ চাপলে মানটি উল্টে যাবে
+isLightOn = !isLightOn; 
+console.log("Light is: " + (isLightOn ? "ON" : "OFF")); // Output: ON
+
+isLightOn = !isLightOn;
+console.log("Light is: " + (isLightOn ? "ON" : "OFF")); // Output: OFF
+
+
+
+let cartItems = []; // শপিং কার্ট খালি
+if (!cartItems.length) {
+    console.log("আপনার কার্টটি খালি! কিছু কেনাকাটা করুন।");
+}
+
+
+let password = "abc123";
+let confirmPassword = "abc456";
+if (password !== confirmPassword) {
+    console.log("পাসওয়ার্ড দুটি মেলেনি! আবার চেষ্টা করুন।");
+}
+
+
+
+// ডেটা লোড হচ্ছে কি না চেক করা (Loading State)
+// ওয়েবসাইটে যখন ডেটা লোড হয়, তখন আমরা একটি স্পিনার (Spinner) দেখাই।
+
+// let isLoading = false;
+// let data = "Some secret info";
+
+// if (!isLoading) {
+//     console.log("Data loaded: " + data);
+// } else {
+//     console.log("Loading... Please wait.");
+// }
+
+// let age = 20;
+// let hasTicket = false;
+// let isVIP = true;
+
+// // যদি টিকিট না থাকে ( !hasTicket ) এবং ভিআইপি-ও না হন ( !isVIP )
+// if (age >= 18 && (hasTicket || isVIP)) {
+//     console.log("Welcome to the club!");
+// } else {
+//     console.log("Access Denied!");
+// }
+
+
+// !true ➡ false
+
+// !false ➡ true
+
+// !"" (খালি লেখা) ➡ true
+
+// !0 (শূন্য) ➡ true
+
+// !"Hello" (কিছু লেখা থাকলে) ➡ false
