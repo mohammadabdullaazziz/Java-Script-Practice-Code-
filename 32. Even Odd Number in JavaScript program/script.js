e8d3abc7a6c09a67a2b43076d22ec9ee0a34ecaf
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-02-28 11:27:09 +0600

    Update script.js

--- 32. Even Odd Number in JavaScript program/script.js
+++ 32. Even Odd Number in JavaScript program/script.js
@@ -7,4 +7,41 @@ if ( (num % 2) == 0) {
     console.log(num + " is Even Number")
 } else {
     console.log(num + " is Odd Number");
+
 }
+
+
+if (input === null) {      // ১ম if শুরু
+    console.log("Input cancelled");
+}                          // ১ম if শেষ
+
+else {                     // else শুরু
+    let num = Number(input);
+
+    if (input.trim() === "" || isNaN(num)) {   // ২য় if শুরু
+        console.log("Please enter a valid number!");
+    }                                          // ২য় if শেষ
+
+    else if (!Number.isInteger(num)) {         // ৩য় if
+        console.log("Please enter an integer number!");
+    } 
+
+    else if (num % 2 === 0) {                  // ৪র্থ if
+        console.log(num + " is an Even Number");
+    } 
+
+    else {                                     // শেষ else
+        console.log(num + " is an Odd Number");
+    }                                          // ভিতরের if-else শেষ
+}                                              // বাইরের else শেষ
+
+
+✔ Cancel detect
+✔ ফাঁকা input detect
+✔ String detect
+✔ Decimal detect
+✔ সুন্দর output format
+✔ Cleaner logic
+-------------------------------------------------------
+let num = Number(prompt("Enter a number: "));
+console.log(num % 2 === 0 ? "Even Number" : "Odd Number");
