d1be306bd3117d2471aa4e1261cbe1e6a7cd7953
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-04-05 14:03:30 +0600

    update again if statement

--- 30. If Else Statement In Javascript/index.js
+++ 30. If Else Statement In Javascript/index.js
@@ -64,3 +64,15 @@ let amount = prompt("Enter Your Amount: ");
 if (amount = "100") {
     console.log("Abdullah");
 }
+
+JavaScript-এ Arithmetic Operators (গাণিতিক অপারেটর যেমন: +, -, *, /) কাজ করার সময় ডাটাকে সাধারণত Number (সংখ্যা) টাইপে কনভার্ট বা রূপান্তর করে নেয়।
+এই প্রক্রিয়াটিকে জাভাস্ক্রিপ্টের ভাষায় বলা হয় Type Coercion।
+
+console.log("10" - 2);   // আউটপুট: 8 (স্ট্রিং "10" সংখ্যা ১০-এ রূপান্তরিত হয়েছে)
+console.log("5" * "4");  // আউটপুট: 20 (দুটি স্ট্রিংই সংখ্যায় রূপান্তরিত হয়েছে)
+console.log(true + 5);   // আউটপুট: 6 (true রূপান্তরিত হয়েছে 1-এ)
+console.log(false + 5);  // আউটপুট: 5 (false রূপান্তরিত হয়েছে 0-এ)
+console.log(null + 10);  // আউটপুট: 10 (null রূপান্তরিত হয়েছে 0-এ)
+console.log(undefined + 10);
+console.log("5" + 2);    // আউটপুট: "52" (এখানে যোগ না হয়ে জোড়া লেগেছে)
+console.log(5 + 2);      // আউটপুট: 7 (সাধারণ যোগ)
