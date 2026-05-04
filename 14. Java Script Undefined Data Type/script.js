e2d4cf08c1d0f724b117221729c53ce73f8461f7
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-04 15:38:09 +0600

    Update Undefined data type details

--- 14. Java Script Undefined Data Type/script.js
+++ 14. Java Script Undefined Data Type/script.js
@@ -1,6 +1,21 @@
-<h3>Undefined Data Type</h3> 
+Undefined Data Type:
+
+জাভাস্ক্রিপ্টে Undefined একটি প্রিমিটিভ (Primitive) ডেটা টাইপ। সহজ কথায়, যখন কোনো ভেরিয়েবল ঘোষণা করা হয় (declare) কিন্তু তাতে কোনো মান (value) সেট করা হয় না, 
+তখন জাভাস্ক্রিপ্ট নিজে থেকেই তাতে undefined বসিয়ে দেয়। 
+
+যখন একটি ভেরিয়েবল তৈরি করা হয় কিন্তু তাকে কোনো ডাটা দেওয়া হয় না, তখন তার মান এবং টাইপ উভয়ই হয় undefined।
 
 let name;
 console.log(name); // Output: undefined
+let nameTwo = undefined;   
 
-let nameTwo = undefined;        
+let myName; 
+console.log(myName); // আউটপুট: undefined
+console.log(typeof myName); // আউটপুট: undefined
+
+
+Undefined বনাম Nullঃ
+
+undefined এবং null এদের মূল পার্থক্য হলো:
+Undefined: মানে হলো জাভাস্ক্রিপ্ট জানে না এর মান কী (অনির্ধারিত)। এটি অটোমেটিক ঘটে।
+Null: মানে হলো আপনি নিজে ইচ্ছাকৃতভাবে ভেরিয়েবলটি খালি রেখেছেন (শূণ্য)।
