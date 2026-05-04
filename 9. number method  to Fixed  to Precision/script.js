96588909f074934b038c03553ea080f25f2800a5
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-04 14:17:36 +0600

    update script with toFixed, toPrecision parseInt...examples
    Added explanations and examples for toFixed and toPrecision methods in Bengali. Included usage of parseInt and parseFloat functions.

--- 9. number method  to Fixed  to Precision/script.js
+++ 9. number method  to Fixed  to Precision/script.js
@@ -1,24 +1,37 @@
-var num = 20;
-num = toString(num);
+JavaScript-এ toFixed() এবং toPrecision() হলো Number অবজেক্টের মেথড, যা সংখ্যা ফরম্যাট (Format) করার জন্য ব্যবহৃত হয়। 
+এগুলোর মাধ্যমে দশমিকের পর কয়টি ঘর থাকবে বা মোট কয়টি সংখ্যা থাকবে তা নির্ধারণ করা যায়।
+
+
+let num = 20.58528;
+console.log(num.toFixed(2));
 console.log(typeof (num));
 
 
-var num = "20";
+let num = 20.58528;
+console.log(num.toPrecision(3))
+console.log(typeof (num));
+
+
+
+let num = 20;
+num = num.toString();
+console.log(num);
+console.log(typeof (num));
+
+জাভাস্ক্রিপ্টে parseInt() এবং parseFloat() হলো স্ট্রিংকে (string) সংখ্যায় রূপান্তর করার ফাংশন parseInt() স্ট্রিং থেকে পূর্ণসংখ্যা (integer) তৈরি করে
+আর parseFloat() দশমিক সংখ্যাসহ (floating-point) মান তৈরি করে [৫]। যেমন, parseInt("10.5") দিলে ১০ হয়, কিন্তু parseFloat("10.5") দিলে ১০.৫ হয় 
+
+let num = "20";
 num = parseInt(num);
+console.log(num);
 console.log(typeof (num));
 
 
-var num = "20.5";
+let num = "20.5";
 num = parseFloat(num);
+console.log(num);
 console.log(typeof (num));
 
-// toFixed 
-var number = 2.2587;
-console.log(number.toFixed(3));
-
-// toPrecision
-var number = 2.2587;
-console.log(number.toPrecision(3));
 
 // number method 
 console.log(Number("12"));
