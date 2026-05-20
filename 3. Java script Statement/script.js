6fcb9dc86fe43a435a24005ec8dcf695adf98a99
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-20 14:06:58 +0600

    Update JavaScript statements

--- 3. Java script Statement/script.js
+++ 3. Java script Statement/script.js
@@ -1,3 +1,11 @@
+যখন সেমিকোলন দাড়া একটি লাইন শেষ হয় তখন তাকে statement বলা হয়
+
+let x = 10;       // স্টেটমেন্ট ১: x নামে একটা বক্স নাও এবং তাতে ১০ রাখো।
+let y = 20;       // স্টেটমেন্ট ২: y নামে আরেকটা বক্স নাও এবং তাতে ২০ রাখো।
+let sum = x + y;  // স্টেটমেন্ট ৩: x আর y যোগ করে sum বক্সে রাখো।
+console.log(sum); // স্টেটমেন্ট ৪: sum এর মানটা স্ক্রিনে দেখাও।
+
+
 alert("Bangladesh World");
 console.log("Abdullah");
 document.write("Abdullah");
